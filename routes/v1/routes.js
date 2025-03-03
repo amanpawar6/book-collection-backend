e365b0fc43b7bfac393d21ecc9bf642bc3f5f60e
Aman Pawar <amanp@iprogrammer.co>
2025-03-03 12:07:25 +0530

    added middleware

--- routes/v1/routes.js
+++ routes/v1/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { addBook, getBooks, deleteBook, getGenres, getBooksByGenres, userBookStatusToggle, userBookStatusRead, userBookStatusUnread, getBookDetails } = require('../../services/bookService');
 const { loginHandler, signupHandler } = require('../../services/userService');
 const upload = require("../../utils/multer");
+const { jwtValidate } = require('../../middlewares/jwt');
 
 const router = express.Router();
 
@@ -11,13 +12,15 @@ router.post('/signup', signupHandler);
 
 router.get("/get-books", getBooks);
 
-router.get("/get-book-details/:id", getBookDetails);
-
-router.post('/add-book', upload.single('coverImage'), addBook);
-
 router.get('/get-genres', getGenres);
 
 router.get('/get-books-by-genre/:genre', getBooksByGenres);
+
+router.get("/get-book-details/:id", getBookDetails);
+
+router.use(jwtValidate);
+
+router.post('/add-book', upload.single('coverImage'), addBook);
 
 router.post('/user-book-status/toggle', userBookStatusToggle);
 
